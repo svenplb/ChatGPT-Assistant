1f594fd368ef29cfcd0c0d9e2f0ab6ea54647d37
alexwloa <alex0907w@gmail.com>
2023-05-07 11:20:40 +0200

    alex_microphone_detection-py_änderungen

--- src/test-code/alex_code/microphone_detection.py
+++ src/test-code/alex_code/microphone_detection.py
@@ -1,87 +1,73 @@
 import os
 import playsound
 import speech_recognition as sr
-# import whisper # hier gibt es einen fehler
 from gtts import gTTS
 
 
 # --------------------------------------------- funktionen ---------------------------------------------#
 def speak():
-    tts = gTTS(text=chatcpt_text, lang="en")
-    # filname localisieren => ein fehler
-    filename = "./files/output.mp3"
+    # tts = gTTS(text=chatgpt_text, lang="de")
     # tts.save(filename)
+    filename = "output.mp3"
     playsound.playsound(filename)
 
 
-# nimmt die audio von dem laptop mic
-def get_auido():
+# takes the audio from the device
+def get_audio():
     r = sr.Recognizer()
-    # microphone anzapfen und hören
     with sr.Microphone() as source:
-        auido = r.listen(source)
-        said = ""
+        r.adjust_for_ambient_noise(source)
+        while True:
+            audio = r.listen(source)
+            try:
+                input_text = r.recognize_google(audio, language="de-DE")
+                # print("Check: ", input_text)
+                if "hey Alex" in input_text:
+                    # print("Command recognized: hey alex")
+                    output_text = input_text.split("hey Alex", 1)[1]
+                    break
+            except sr.RequestError as error:
+                print("Error", error)
+                break
 
-        try:
-            said = r.recognize_google(auido)
-            print(said)
-        except Exception as error:
-            print("Exeption " + str(error))
-    return said
+    return output_text
 
 
 # whisper funktionen
 def text_to_speech():
-    with open("./files/chatcpt_output.txt", 'r') as string:
+    with open("chatgpt_output.txt", 'r') as string:
         input_text = string.read()
-    tts = gTTS(input_text)
-    tts.save('./files/output.mp3')
+    tts = gTTS(input_text, lang="de")
+    tts.save('output.mp3')
     speak()
 
 
-"""
-def speech_to_text():
-    # tiny, base, small, medium, large einstellbar
-    model = whisper.load_model("base")
-    options = {"language": "de"}
-
-    res = model.transcribe(
-        "", **options)
-
-    print(res["text"])
-"""
-
 # --------------------------------------------- main code ---------------------------------------------#
-print("Anfang des codes")
 
+# Infinite loop to wait for voice input
+# while True:
 try:
-    text = get_auido()
-    # Open file in schreib modus
-    # überprüfen ob die eingabe stimmt
-    with open(".\files\output.txt", "w") as f:
-        # Reinschreiben was man gesagt hat
-        f.write(text)
-
+    main_text = get_audio()
+    kurz_geschreiben = "in einem satz"
+    main_text = " ".join((main_text, kurz_geschreiben))
+    # Open file in write mode
+    with open("output.txt", "w") as f:
+        f.write(main_text)
 except Exception as e:
     print(e)
 
-# classe chatcpt den text geben und chat cpt fragen mit dem text
+# give classe chatcpt the text and ask chat cpt for the answer
 try:
-    # andere klasse
     import chatgpt
-    # hier zur andern klasse dann den chatcpt text zurück geben
-    # hier ist ein fehler und ich weiß nicht wie es geht mit den klassen
-    chatcpt_text = chatgpt.output(text)
-    print(chatcpt_text)
-    # Open file in schreib moduss
-    # überprüfen ob die eingabe stimmt
-    with open("./files/chatcpt_output.txt", "w") as f:
-        # Reinschreiben was man gesagt hat
-        f.write(chatcpt_text)
-
+    chatgpt_text = chatgpt.output(main_text)
+    print(chatgpt_text)
+    # Open file in write mode
+    with open("chatgpt_output.txt", "w") as f:
+        f.write(chatgpt_text)
 except Exception as e:
     print(e)
 
-# die nachricht welche man von chatcpt erhalten hat text_to_speech
+# the message you received from chatgpt text_to_speech
 text_to_speech()
+
 print("Ende des Programmes")
